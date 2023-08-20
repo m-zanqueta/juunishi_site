79c661bc3051b4f9bcff85b1e661088d9506ed66
pbielz <pbiel.muniz@gmail.com>
2023-08-20 17:47:49 -0300

    Initial commit

---
+++ js/principal.js
@@ -0,0 +1,37 @@
+/*Menu Hamburguer*/
+
+function abrir_Menu() 
+{
+	document.getElementById("aberto").className = document.getElementById("aberto").className.replace("mostrar_hamb","esconder_hamb");
+	document.getElementById("fechado").className = document.getElementById("fechado").className.replace("esconder_hamb","mostrar_hamb");
+    document.getElementById("menu_lateral").className = document.getElementById("menu_lateral").className.replace("fechar_menu","abrir_menu");
+}
+
+function fechar_Menu()
+{
+	
+	document.getElementById("fechado").className = document.getElementById("fechado").className.replace("mostrar_hamb","esconder_hamb");
+	document.getElementById("aberto").className = document.getElementById("aberto").className.replace("esconder_hamb","mostrar_hamb");
+    document.getElementById("menu_lateral").className = document.getElementById("menu_lateral").className.replace("abrir_menu","fechar_menu");
+}
+
+
+
+
+var contador=1;
+var img1="../Img/ci1.jpg";
+var img2="../Img/ci2.jpg";
+var img3="../Img/ci3.jpg";
+var img4="../Img/ci4.jpg";
+var tempo=4100;
+var exibir=setInterval("Exibindo()", tempo);
+
+function Exibindo()
+{    
+    if(contador<4)
+        contador++;
+    else
+        contador=1;
+
+    document.images["slide"].src=eval("img"+contador);
+}
